7783ed6781b09e1750bb22d76bae86cd8d6b2808
Muhammet DÜZENLİ <73781096+muhammetduzenli@users.noreply.github.com>
2022-08-25 07:29:37 +0300

    Update SurveySaurusLogin.py

--- SurveySaurusLogin.py
+++ SurveySaurusLogin.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
 import time
+service = Service(".\\chromedriver.exe")
 
-driver = webdriver.Chrome(service = service)
 driver.maximize_window()
 
 def login(username, password):
