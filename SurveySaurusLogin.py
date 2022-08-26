8bb5317b2b20e8d1223b9269ed40321bb9f2eb85
Muhammet DÜZENLİ <73781096+muhammetduzenli@users.noreply.github.com>
2022-08-26 11:59:40 +0300

    Update SurveySaurusLogin.py

--- SurveySaurusLogin.py
+++ SurveySaurusLogin.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('--disable-infobars')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+options.add_argument('--remote-debugging-port=9222')
+driver = webdriver.Chrome(options=options)
 driver = webdriver.Chrome('/usr/bin/google-chrome',chrome_options=chrome_options)
 #driver = webdriver.Chrome(ChromeDriverManager().install())
 import time
