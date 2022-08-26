58e1460781503fbff9d1a1f62b16a44387bc8d37
Muhammet DÜZENLİ <73781096+muhammetduzenli@users.noreply.github.com>
2022-08-26 12:26:28 +0300

    Update SurveySaurusLogin1.py

--- SurveySaurusLogin1.py
+++ SurveySaurusLogin1.py
@@ -7,7 +7,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 #options.add_argument('--disable-infobars')
 #options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
+#options.add_argument('--no-sandbox')
 #options.add_argument('--remote-debugging-port=9222')
 driver = webdriver.Chrome(options=options)
 
