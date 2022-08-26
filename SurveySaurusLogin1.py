8208c48c1e571769bec28f16c6d00cc4d70d6c5b
Muhammet DÜZENLİ <73781096+muhammetduzenli@users.noreply.github.com>
2022-08-26 12:27:21 +0300

    Update SurveySaurusLogin1.py

--- SurveySaurusLogin1.py
+++ SurveySaurusLogin1.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+#options.add_argument('headless')
 #options.add_argument('--disable-infobars')
 #options.add_argument('--disable-dev-shm-usage')
 #options.add_argument('--no-sandbox')
